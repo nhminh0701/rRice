c76cef0610ee948fcd91c6bdea70596f120ea325
Unknown <parisantoine78@gmail.com>
2017-08-18 15:07:25 +0700

    ##

--- R/linkPython-functions.R
+++ R/linkPython-functions.R
@@ -15,22 +15,7 @@ callExecutablePy <- function () {
     
     path2 <- system.file("python",
                         package = "rRice")
-    
-    #print(paste0("R : ",path))
-    #capture.output(path, file = "python/appel.txt")
-    #modif = readLines(path1, -1)
-    #modif[1] = path
-    #writeLines(path, path1)
-    
-    ##put the variable path into python __main__
-    # if (Sys.getenv("R_ARCH") == "/x64") {
-    #   PythonInR::pyConnect("C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python35\\python.exe")
-    #   
-    # }
-    # else {
-    #   PythonInR::pyConnect("C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python35-32\\python.exe")
-    # }
-    # print(PythonInR::pyIsConnected())
+
     PythonInR::pySet("path", path2, namespace = "__main__")
     PythonInR::pyExecfile(path)
 }
