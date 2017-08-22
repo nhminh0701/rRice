86d4e97ed88bb548d3e40d8df1202eb039de69b8
Unknown <parisantoine78@gmail.com>
2017-08-22 15:39:08 +0700

    updates

--- inst/python/importmodules.py
+++ inst/python/importmodules.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
+import importlib
 import pip
 
 def install(package):
     pip.main(['install', package])
 
 def main():
-    try:
-        #install("pandas")
-        #install("bs4")
-        #install("requests")
-        #install("json")
-        #install("gzip")
-        #install("lxml")
-    except:
-        pass
+    if (importlib.find_loader('pandas') is None) :
+        install("pandas")
+    if (importlib.find_loader('bs4') is None) :
+        install("bs4")
+    if (importlib.find_loader('requests') is None) :
+        install("requests")
+    if (importlib.find_loader('json') is None) :
+        install("json")
+    if (importlib.find_loader('gzip') is None) :
+        install("gzip")
+    if (importlib.find_loader('lxml') is None) :
+        install("lxml")
 
 
 # Pour eviter que le script soit execute lors d'un simple import
