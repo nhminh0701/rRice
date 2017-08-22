609cc4b13e83ba032e3b51601787a238803b745c
Unknown <parisantoine78@gmail.com>
2017-08-22 14:25:32 +0700

    travis tests

--- inst/python/importmodules.py
+++ inst/python/importmodules.py
@@ -7,35 +7,35 @@ def install(package):
     pip.main(['install', package])
 
 def main():
-    try:
-        imp.find_module('pandas')
-    except ImportError:
-        install("pandas")
-
-    try:
-        imp.find_module('bs4')
-    except ImportError:
-        install("bs4")
-    
-    try:
-        imp.find_module('requests')
-    except ImportError:
-        install("requests")
-    
-    try:
-        imp.find_module('json')
-    except ImportError:
-        install("json")
-
-    try:
-        imp.find_module('gzip')
-    except ImportError:
-        install("gzip")
-    
-    try:
-        imp.find_module('lxml')
-    except ImportError:
-        install("lxml")
+    # try:
+    #     imp.find_module('pandas')
+    # except ImportError:
+    #     install("pandas")
+    # 
+    # try:
+    #     imp.find_module('bs4')
+    # except ImportError:
+    #     install("bs4")
+    # 
+    # try:
+    #     imp.find_module('requests')
+    # except ImportError:
+    #     install("requests")
+    # 
+    # try:
+    #     imp.find_module('json')
+    # except ImportError:
+    #     install("json")
+    # 
+    # try:
+    #     imp.find_module('gzip')
+    # except ImportError:
+    #     install("gzip")
+    # 
+    # try:
+    #     imp.find_module('lxml')
+    # except ImportError:
+    #     install("lxml")
 
     
 # Pour eviter que le script soit execute lors d'un simple import
