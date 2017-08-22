3b28bacf0a5ec4e4f7f8b16fdd58bebd9666d799
Unknown <parisantoine78@gmail.com>
2017-08-22 15:13:01 +0700

    test

--- inst/python/importmodules.py
+++ inst/python/importmodules.py
@@ -7,12 +7,13 @@ def install(package):
     pip.main(['install', package])
 
 def main():
-    install("pandas")
-    install("bs4")
-    install("requests")
-    install("json")
-    install("gzip")
-    install("lxml")
+    try:
+        install("pandas")
+        install("bs4")
+        install("requests")
+        install("json")
+        install("gzip")
+        install("lxml")
 
 
 # Pour eviter que le script soit execute lors d'un simple import
