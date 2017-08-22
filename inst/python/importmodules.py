a7f4bbec0ca8c30068e12b277930c9c17dc10ced
Unknown <parisantoine78@gmail.com>
2017-08-22 14:58:58 +0700

    travis test

--- inst/python/importmodules.py
+++ inst/python/importmodules.py
@@ -7,18 +7,12 @@ def install(package):
     pip.main(['install', package])
 
 def main():
-    if (importlib.find_loader('pandas') is None) :
-        install("pandas")
-    if (importlib.find_loader('bs4') is None) :
-        install("bs4")
-    if (importlib.find_loader('requests') is None) :
-        install("requests")
-    if (importlib.find_loader('json') is None) :
-        install("json")
-    if (importlib.find_loader('gzip') is None) :
-        install("gzip")
-    if (importlib.find_loader('lxml') is None) :
-        install("lxml")
+    install("pandas")
+    install("bs4")
+    install("requests")
+    install("json")
+    install("gzip")
+    install("lxml")
 
 
 # Pour eviter que le script soit execute lors d'un simple import
