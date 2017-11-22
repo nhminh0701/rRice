6fdb948413594c8abdd526fe5b5fc7172f938dcf
lamdv <lam.dv@live.com>
2017-11-22 11:01:18 +0700

    Add documentation

--- inst/python/run.py
+++ inst/python/run.py
@@ -23,137 +23,143 @@ import planttfdb as planttfdb
 
 def main():
     pathScript = sys.argv[0]
-    contig = sys.argv[1]
-    if len(contig) < 2:
-        contig = 'chr0' + contig  # test if for 10 - 11 - 12
-    else:
-        contig = 'chr' + contig
-    start = sys.argv[2]
-    end = sys.argv[3]
-    db = sys.argv[4]
+    db = sys.argv[1]
+    print(query.query(db, sys.argv[2:]))
 
-    dataSnp = query.query("snpseek", [contig, start, end, "msu7"])
 
-    id = sys.argv[5]
+###### Legacy Code ######
+# def main():
+#     pathScript = sys.argv[0]
+#     contig = sys.argv[1]
+#     if len(contig) < 2:
+#         contig = 'chr0' + contig  # test if for 10 - 11 - 12
+#     else:
+#         contig = 'chr' + contig
+#     start = sys.argv[2]
+#     end = sys.argv[3]
+#     db = sys.argv[4]
 
-    if (db == "1"):
-        dataRapdb = query.query("rapdb", [id])
-        print(dataRapdb)
+#     dataSnp = query.query("snpseek", [contig, start, end, "msu7"])
 
-    elif (db == "call_snpSeek"):
-        for i in range(0, len(dataSnp)):
-            print(dataSnp[i])
+#     id = sys.argv[5]
 
-    elif (db == "2"):
-        dataGramene = query.query("gramene", [id])
-        print(dataGramene)
+#     if (db == "1"):
+#         dataRapdb = query.query("rapdb", [id])
+#         print(dataRapdb)
 
-    elif (db == "3"):
-        dataOryzabase = query.query("oryzabase", [id])
-        print(dataOryzabase)
+#     elif (db == "call_snpSeek"):
+#         for i in range(0, len(dataSnp)):
+#             print(dataSnp[i])
 
-    elif (db == "4"):
-        query.query('ic4r', [id])
+#     elif (db == "2"):
+#         dataGramene = query.query("gramene", [id])
+#         print(dataGramene)
 
-    elif (db == "5"):
-        dataPlanttfdb = planttfdb.planttfdb([id])
-        print(dataPlanttfdb)
+#     elif (db == "3"):
+#         dataOryzabase = query.query("oryzabase", [id])
+#         print(dataOryzabase)
 
-    # LOC_xxxxxxxx
-    elif (db == "6"):
-        dataPlntfdb = query.query("plntfdb",[id])
-        print(dataPlntfdb)
+#     elif (db == "4"):
+#         query.query('ic4r', [id])
 
-    elif (db == "7"):
-        dataFunricegenes = query.query("funricegene_genekeywords",[id])
-        print(dataFunricegenes)
+#     elif (db == "5"):
+#         dataPlanttfdb = planttfdb.planttfdb([id])
+#         print(dataPlanttfdb)
 
-    elif (db == "8"):
-        dataFunricegenes2 = query.query("funricegene_geneinfo",[id])
-        print(dataFunricegenes2)
+#     # LOC_xxxxxxxx
+#     elif (db == "6"):
+#         dataPlntfdb = query.query("plntfdb",[id])
+#         print(dataPlntfdb)
 
-    elif (db == "9"):
-        dataFunricegenes3 = query.query("funricegene_faminfo",[id])
-        print(dataFunricegenes3)
+#     elif (db == "7"):
+#         dataFunricegenes = query.query("funricegene_genekeywords",[id])
+#         print(dataFunricegenes)
 
-    elif (db == "10"):
-        dataMsu = query.query("msu",[id])
-        print(dataMsu)
+#     elif (db == "8"):
+#         dataFunricegenes2 = query.query("funricegene_geneinfo",[id])
+#         print(dataFunricegenes2)
 
-        # Ecriture fichier a revoir !!!!!!!!! pour les id et hashmap[iricname] et hashmpap [raprepname]
-    elif (db == "13"):
-        url = "http://rapdb.dna.affrc.go.jp/download/archive/RAP-MSU_2017-04-14.txt.gz"
-        filename = url.split("/")[-1]
+#     elif (db == "9"):
+#         dataFunricegenes3 = query.query("funricegene_faminfo",[id])
+#         print(dataFunricegenes3)
 
-        # Give the name of the file without .gz
-        pathToFile = helper.formatPathToFile(filename[:-3])
+#     elif (db == "10"):
+#         dataMsu = query.query("msu",[id])
+#         print(dataMsu)
 
-        if (not os.path.isfile(pathToFile)):
-            # Fetch the file by the url and decompress it
-            r = requests.get(url)
-            decompressedFile = gzip.decompress(r.content)
-            # Create the file .txt
-            with open(pathToFile, "w") as f:
-                f.write(decompressedFile)
-                f.close()
-        newFile = helper.formatPathToFile("geneID.txt")
-        with open(newFile, "a") as f:
-            # Import file tab-delimited
-            try:
-                array = pd.read_csv(pathToFile, sep="\t", header=None)
-            except:
-                array = pd.DataFrame()
-            # Named columns
-            array.columns = ["RAP", "LOC"]
+#         # Ecriture fichier a revoir !!!!!!!!! pour les id et hashmap[iricname] et hashmpap [raprepname]
+#     elif (db == "13"):
+#         url = "http://rapdb.dna.affrc.go.jp/download/archive/RAP-MSU_2017-04-14.txt.gz"
+#         filename = url.split("/")[-1]
 
-            # Find the line corresponding to the entered RAP ID (Select LOC FROM LOC where RAP = RapID)
-            data = array.loc[array['RAP'] == id]
-            #data.loc[:, 'iricname'] = hashmap['iricname']
+#         # Give the name of the file without .gz
+#         pathToFile = helper.formatPathToFile(filename[:-3])
 
-            # Store the corresponding LOC ID and split the string
-            print(data['iricname'])
-            data.to_csv(f, sep='\t')
+#         if (not os.path.isfile(pathToFile)):
+#             # Fetch the file by the url and decompress it
+#             r = requests.get(url)
+#             decompressedFile = gzip.decompress(r.content)
+#             # Create the file .txt
+#             with open(pathToFile, "w") as f:
+#                 f.write(decompressedFile)
+#                 f.close()
+#         newFile = helper.formatPathToFile("geneID.txt")
+#         with open(newFile, "a") as f:
+#             # Import file tab-delimited
+#             try:
+#                 array = pd.read_csv(pathToFile, sep="\t", header=None)
+#             except:
+#                 array = pd.DataFrame()
+#             # Named columns
+#             array.columns = ["RAP", "LOC"]
 
-            f.close()
+#             # Find the line corresponding to the entered RAP ID (Select LOC FROM LOC where RAP = RapID)
+#             data = array.loc[array['RAP'] == id]
+#             #data.loc[:, 'iricname'] = hashmap['iricname']
 
-    # Plage chromosome
-    # Cree le fichier fileID.txt
-    elif (db == "11"):
-        query.query("snpseek", ["chr12", '1', "27531856", "rap"])
-        query.query("snpseek", ["chr02", "1", "35937250", "rap"])
-        query.query("snpseek", ["chr03", "1", "36413819", "rap"])
-        query.query("snpseek", ["chr04", "1", "35502694", "rap"])
-        query.query("snpseek", ["chr05", "1", "29958434", "rap"])
-        query.query("snpseek", ["chr06", "1", "31248787", "rap"])
-        query.query("snpseek", ["chr07", "1", "29697621", "rap"])
-        query.query("snpseek", ["chr08", "1", "28443022", "rap"])
-        query.query("snpseek", ["chr09", "1", "23012720", "rap"])
-        query.query("snpseek", ["chr10", "1", "23207287", "rap"])
-        query.query("snpseek", ["chr11", "1", "29021106", "rap"])
-        query.query("snpseek", ["chr12", "1", "27531856", "rap"])
+#             # Store the corresponding LOC ID and split the string
+#             print(data['iricname'])
+#             data.to_csv(f, sep='\t')
 
-    # Return the SnpSeek Call
-    elif (db == "12"):
-        print(dataSnp)
+#             f.close()
 
-    """
-    elif (db == "3"):
-        try:
-            dataOryzabase = oryzabase.oryzabaseRapId(id)
-            print(dataOryzabase)
-        except:
-            # empty error
-            print("Rap ID not found")
-            try:
-                rapdbCGSNL = rapdb.rapdb(id)
-                dataOryzabase = oryzabase.oryzabaseCGSNL(rapdbCGSNL["CGSNL Gene Name"])
-                print(dataOryzabase)
-            except:
-                print("Not found")
-    """
+#     # Plage chromosome
+#     # Cree le fichier fileID.txt
+#     elif (db == "11"):
+#         query.query("snpseek", ["chr12", '1', "27531856", "rap"])
+#         query.query("snpseek", ["chr02", "1", "35937250", "rap"])
+#         query.query("snpseek", ["chr03", "1", "36413819", "rap"])
+#         query.query("snpseek", ["chr04", "1", "35502694", "rap"])
+#         query.query("snpseek", ["chr05", "1", "29958434", "rap"])
+#         query.query("snpseek", ["chr06", "1", "31248787", "rap"])
+#         query.query("snpseek", ["chr07", "1", "29697621", "rap"])
+#         query.query("snpseek", ["chr08", "1", "28443022", "rap"])
+#         query.query("snpseek", ["chr09", "1", "23012720", "rap"])
+#         query.query("snpseek", ["chr10", "1", "23207287", "rap"])
+#         query.query("snpseek", ["chr11", "1", "29021106", "rap"])
+#         query.query("snpseek", ["chr12", "1", "27531856", "rap"])
 
-    # geneID(contig, start, end, hashmap[], hashmap["raprepName"])
+#     # Return the SnpSeek Call
+#     elif (db == "12"):
+#         print(dataSnp)
 
+#     """
+#     elif (db == "3"):
+#         try:
+#             dataOryzabase = oryzabase.oryzabaseRapId(id)
+#             print(dataOryzabase)
+#         except:
+#             # empty error
+#             print("Rap ID not found")
+#             try:
+#                 rapdbCGSNL = rapdb.rapdb(id)
+#                 dataOryzabase = oryzabase.oryzabaseCGSNL(rapdbCGSNL["CGSNL Gene Name"])
+#                 print(dataOryzabase)
+#             except:
+#                 print("Not found")
+#     """
+
+#     # geneID(contig, start, end, hashmap[], hashmap["raprepName"])
 
 
 # Pour eviter que le script soit execute lors d'un simple import
