f32072c8ac06ab2c197f67001d9612abba0c1ac2
Pierre Larmande <pierre.larmande@ird.fr>
2017-12-15 12:56:26 +0700

    fixing chromosome coordinate

--- inst/python/run.py
+++ inst/python/run.py
@@ -115,7 +115,7 @@ def main():
             data.to_csv(f, sep='\t')
 
             f.close()
-    # test branch - 2 
+    # test branch - 2
     # Plage chromosome
     # Cree le fichier fileID.txt
     elif (db == "11"):
